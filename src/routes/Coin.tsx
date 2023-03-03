025bec58e1e86f3c4d6229d5b0368862ed12bb20
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-03-03 20:34:30 +0900

    Route coin page(3)
    typescript에게 info와 priceInfo를 설명해줌 (interface)

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -34,12 +34,68 @@ interface RouteState {
   name: string;
 }
 
+interface infoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface priceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<infoData>();
+  const [priceInfo, setPriceInfo] = useState<priceData>();
 
   /* useEffect 컴포넌트가 렌더링 될 때 
       특정 작업을 실행할 수 있도록 하는 Hook이다 */
@@ -48,9 +104,11 @@ function Coin() {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json(); /* 코인 정보 링크 */
+
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json(); /* 코인 가격 정보 링크 */
+
       setInfo(infoData);
       setPriceInfo(priceData);
     })();
