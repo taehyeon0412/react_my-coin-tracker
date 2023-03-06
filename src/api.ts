970848ffa4b7f54e714649e8169baf4368cb5780
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-03-06 21:01:47 +0900

    chart(1)

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
+const NOMAD_URL = `https://ohlcv-api.nomadcoders.workers.dev/?coinId=`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
@@ -16,6 +17,12 @@ export function fetchCoinPrice(coinId: string) {
     response.json()
   );
 } /* 코인 가격 정보 */
+
+export function fetchCoinHistory(coinId: string) {
+  /* const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7; */
+  return fetch(`${NOMAD_URL}${coinId}`).then((response) => response.json());
+}
 
 /*API를 fetch하고 json을 리턴해준다
   return fetch(`주소`).then((response)=>response.json());
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,19 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery(["chartUpDown", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   return <h1>Chart</h1>;
 }
 
 export default Chart;
+
+/* 1.우리가 보고자 하는 암호화폐가 무엇인지 알아야됨 
+   2.coinId props를 차트페이지로 보내준다
+*/
