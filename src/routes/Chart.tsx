145f0df7ac6e3329a57f6ca0acc1e9cb94be58f0
taehyeon0412 <71374539+taehyeon0412@users.noreply.github.com>
2023-03-07 00:05:22 +0900

    chart(2)
    차트 데이터 시각화

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,56 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface ChartData {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["chartUpDown", coinId], () =>
-    fetchCoinHistory(coinId)
+  const { isLoading, data } = useQuery<ChartData[]>(
+    ["chartUpDown", coinId],
+    () => fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              /* close 종가만 차트로 그림 */
+              data: data?.map((price) => price.close) as number[],
+            },
+          ]}
+          /* data가 받아야되는건 number이고 data?.map()으로 읽어올때와
+                 읽어오지 못할때를 구분해서 받아야되는데 읽어오면 number이지만
+                 못읽어오면 undefined 형태가 되서 문제가 생김
+                 그래서 as를 이용하여 number배열 형태로 강제해줘서 오류를 잡음 */
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: { height: 300, width: 500, toolbar: { show: false } },
+            stroke: { curve: "smooth", width: 4 },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
